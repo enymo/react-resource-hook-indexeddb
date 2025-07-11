31ca0b082dfea75f92b26c5ca756d7d384ba6142
Gideon Ritter <gideon.ritter@enymo.com>
2025-07-11 14:52:27 +0200

    work on offline support

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,24 +1,35 @@
-import type { Resource, ResourceBackendAdapter } from "@enymo/react-resource-hook";
+import type { Delta, DeltaResourceBackendAdapter, Resource } from "@enymo/react-resource-hook";
 import Dexie, { EntityTable, Transaction } from "dexie";
-import { useMemo } from "react";
-import { v4 as uuidv4 } from "uuid";
+import { useLiveQuery } from "dexie-react-hooks";
+import { useCallback } from "react";
+
+const methodNotSupported = () => {
+    throw new Error("Method not supported");
+}
 
 export default function createIndexedDBResourceAdapter({
     databaseName,
-    generateUuids,
     schema
 }: {
     databaseName: string,
-    generateUuids?: boolean,
     schema: {
         version: number,
         schema: {[table: string]: string},
         upgrade?: (trans: Transaction) => PromiseLike<any> | void
     }[]
-}): ResourceBackendAdapter<{}, {}, never, never> {
+}): DeltaResourceBackendAdapter<{}, {}, never, never> {
     const db = new Dexie(databaseName) as Dexie & {
         [table: string]: EntityTable<Resource, "id">
     };
+    const deltaDb = new Dexie(`${databaseName}--deltas`) as Dexie & {
+        deltas: EntityTable<Delta<Resource> & {
+            resource: string
+        }, any>
+    };
+    
+    deltaDb.version(1).stores({
+        deltas: "[resource+id]"
+    });
 
     for (const entry of schema) {
         const version = db.version(entry.version).stores(entry.schema);
@@ -28,48 +39,46 @@ export default function createIndexedDBResourceAdapter({
     }
 
     return resource => ({
-        actionHook: () => useMemo(() => ({
-            store: async data => {
-                const insert = {
-                    ...data,
-                    id: generateUuids ? uuidv4() : data.id
-                };
-                await db[resource].add(insert);
-                return insert;               
-            },
-            batchStore: async data => {
-                await Promise.all(data.map(item => db[resource].add({
-                    ...item,
-                    id: generateUuids ? uuidv4 : item.id
-                })));
+        actionHook: () => ({
+            store: useCallback(async data => {
+                await db[resource].add(data);
+                return data;               
+            }, [resource]),
+            batchStore: useCallback(async data => {
+                await Promise.all(data.map(item => db[resource].add(item)));
                 return data;
-            },
-            update: async (id, data) => {
+            }, [resource]),
+            update: useCallback(async (id, data) => {
                 await db[resource].update(id, data);
-                return db[resource].get(id);
-            },
-            batchUpdate: async data => {
+                return db[resource].get(id) as any;
+            }, [resource]),
+            batchUpdate: useCallback(async data => {
                 await Promise.all(data.map(item => {
                     const {id, ...rest} = item;
                     return db[resource].update(id, rest);
                 }));
-                return db[resource].where("id").anyOf(data.map(item => item.id)).toArray();
-            },
-            destroy: async id => {
+                return db[resource].where("id").anyOf(data.map(item => item.id)).toArray() as any;
+            }, [resource]),
+            destroy: useCallback(async id => {
                 await db[resource].delete(id)
-            },
-            batchDestroy: async ids => {
+            }, [resource]),
+            batchDestroy: useCallback(async ids => {
                 await Promise.all(ids.map(id => db[resource].delete(id)))
-            },
-            query: () => {
-                throw new Error("Method not supported")
-            },
-            refresh: async id => ({
+            }, [resource]),
+            query: methodNotSupported,
+            refresh: useCallback(async id => ({
                 data: await (id !== undefined ? db[resource].get(id) : db[resource].toArray()) as any,
                 meta: undefined as any,
                 error: null
-            })
-        }), [resource]),
+            }), [resource]),
+            deltas: useLiveQuery(() => deltaDb.deltas.where("resource").equals(resource).toArray() as any, [resource]),
+            writeDelta: useCallback(delta => deltaDb.deltas.put({
+                ...delta,
+                resource
+            }), [resource]),
+            flushDelta: useCallback(id => deltaDb.deltas.delete([resource, id]), [resource]),
+            offline: false
+        }),
         eventHook: () => {}
     })
 }
